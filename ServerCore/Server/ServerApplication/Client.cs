5861cf082a9d8ce4aacddadc9b12ed3f277300b7
Wojciech Zielonka <g36@wp.pl>
2016-05-11 23:01:45 +0200

    Client class

---
+++ ServerCore/Server/ServerApplication/Client.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerApplication
+{
+    class Client
+    {
+        public string HashCode { get; set; }
+
+        public string UserName { get; set; }
+
+        public IPAddress IpAddress { get; set; }
+
+        public double Lat { get; set; }
+
+        public double Lon { get; set; }
+
+        public ChatMessage Message { get; set; }
+
+    }
+}
